293ecf7114e18829fd7f6fba03c43f91d76041f9
madhu sudhan <mmudiraj90@gmail.com>
2021-05-09 12:28:29 +0530

    Refactor code for custom exception

--- main/java/UserRegistration.java
+++ main/java/UserRegistration.java
@@ -9,32 +9,58 @@ public class UserRegistration {
                                             "@([a-zA-z0-9][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
         private static final String numberPattern = "[0-9]{2}\\s[0-9]{10}";
 
-        public boolean firstNameValidation(String firstname) {
-            Pattern pattern = Pattern.compile(firstNamePattern);
-            return pattern.matcher(firstname).matches();
+
+            public boolean firstNameValidation(String firstname) throws UserRegistrationException {
+                boolean result = Pattern.compile(firstNamePattern).matcher(firstname).matches();
+                if (result) {
+                    System.out.println("firstname is valid");
+                    return true;
+                } else {
+                    throw new UserRegistrationException("firstname is not valid");
+                }
+            }
+
+              public boolean lastNameValidation(String lastname) throws UserRegistrationException {
+                    boolean result = Pattern.compile(lastNamePattern).matcher(lastname).matches();
+                    if (result) {
+                        System.out.println("lastname is valid");
+                        return true;
+                    } else {
+                        throw new UserRegistrationException("lastname is not valid");
+                    }
+                }
+
+                    public boolean passwordValidation(String password) throws UserRegistrationException {
+                        boolean result = Pattern.compile(passwordPattern).matcher(password).matches();
+                        if (result) {
+                            System.out.println("password is valid");
+                            return true;
+                        } else {
+                            throw new UserRegistrationException("password is not valid");
+                        }
         }
 
+                        public boolean emailValidation (String email) throws UserRegistrationException {
+                            boolean result = Pattern.compile(emailPattern).matcher(email).matches();
 
-        public boolean lastNameValidation(String lastname) {
-        Pattern pattern = Pattern.compile(lastNamePattern);
-        return pattern.matcher(lastname).matches();
+
+                            if (result) {
+                                System.out.println("email is valid");
+                                return true;
+                            } else {
+                                throw new UserRegistrationException("email is not valid");
+                            }
+                        }
+
+
+                            public boolean numberValidation (String number) throws UserRegistrationException {
+                                boolean result = Pattern.compile(numberPattern).matcher(number).matches();
+                                if (result) {
+                                    System.out.println("number is valid");
+                                    return true;
+                                } else {
+                                    throw new UserRegistrationException("number is not valid");
+                                }
+                            }
        }
-
-        public boolean passwordValidation(String password) {
-            Pattern pattern = Pattern.compile(passwordPattern);
-            return pattern.matcher(password).matches();
-        }
-
-        public boolean emailValidation(String email) {
-        Pattern pattern = Pattern.compile(emailPattern);
-        return pattern.matcher(email).matches();
-    }
-
-        public boolean numberValidation(String number) {
-        Pattern pattern = Pattern.compile(numberPattern);
-        return pattern.matcher(number).matches();
-    }
-
-
-    }
 
--- test/java/UserRegistrationTest.java
+++ test/java/UserRegistrationTest.java
@@ -3,35 +3,54 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
         @Test
-        public void checkFirstName() {
-            UserRegistration userRegistration = new UserRegistration();
-            boolean result = userRegistration.firstNameValidation("Jeff");
-            Assert.assertEquals(true, result);
+        public void givenFirstName_WhennotValid_ShouldThrowException() throws UserRegistrationException {
+            try {
+                UserRegistration userRegistration = new UserRegistration();
+                boolean result = userRegistration.firstNameValidation("Jeff");
+                Assert.assertEquals(true, result);
+            } catch (UserRegistrationException e) {
+                e.printStackTrace();
+            }
         }
-
     @Test
-    public void checkLastName() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.lastNameValidation("Bezos");
-        Assert.assertEquals(true, result);
+    public void givenLaststName_WhennotValid_ShouldThrowException() throws UserRegistrationException {
+        try {
+            UserRegistration userRegistration = new UserRegistration();
+            boolean result = userRegistration.lastNameValidation("Bezos");
+            Assert.assertEquals(true, result);
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
     }
     @Test
-    public void checkPassword() {
+    public void  givenPassword_WhennotValid_ShouldThrowException() throws UserRegistrationException {
+        try {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.passwordValidation("Madhu@123");
         Assert.assertEquals(true, result);
+    }  catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
     }
     @Test
-    public void checkEmail() {
+    public void givenEmail_WhennotValid_ShouldThrowException() throws UserRegistrationException {
+        try {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.emailValidation("abc.xyz@bl.com.in");
         Assert.assertEquals(true, result);
+    } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
     }
     @Test
-    public void checkNumber() {
+    public void givenNumber_WhennotValid_ShouldThrowException() throws UserRegistrationException {
+        try {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.numberValidation("91 8247473242");
         Assert.assertEquals(true, result);
+    }  catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
     }
 
     }
